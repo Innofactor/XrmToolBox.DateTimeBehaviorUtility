d4c10a468c645517c12211ea34ddb78c179970ec
Jonas Rapp <jonas.rapp@cinteros.se>
2016-10-05 12:36:32 +0200

    Added some UI and method stubs.

--- Innofactor.XTB.DateTimeBehaviorUtility/DateTimeBehaviorUtility.Designer.cs
+++ Innofactor.XTB.DateTimeBehaviorUtility/DateTimeBehaviorUtility.Designer.cs
@@ -31,16 +31,23 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DateTimeBehaviorUtility));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.btnClose = new System.Windows.Forms.ToolStripButton();
+            this.btnLoadAttributes = new System.Windows.Forms.ToolStripButton();
+            this.pnlAttributes = new System.Windows.Forms.Panel();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
             this.toolStrip1.SuspendLayout();
+            this.pnlAttributes.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
             // 
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.btnClose});
+            this.btnClose,
+            this.btnLoadAttributes});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(404, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(907, 25);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -53,15 +60,69 @@
             this.btnClose.Text = "Close";
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
+            // btnLoadAttributes
+            // 
+            this.btnLoadAttributes.Enabled = false;
+            this.btnLoadAttributes.Image = ((System.Drawing.Image)(resources.GetObject("btnLoadAttributes.Image")));
+            this.btnLoadAttributes.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnLoadAttributes.Name = "btnLoadAttributes";
+            this.btnLoadAttributes.Size = new System.Drawing.Size(108, 22);
+            this.btnLoadAttributes.Text = "Load Attributes";
+            this.btnLoadAttributes.Click += new System.EventHandler(this.btnLoadAttributes_Click);
+            // 
+            // pnlAttributes
+            // 
+            this.pnlAttributes.Controls.Add(this.label1);
+            this.pnlAttributes.Controls.Add(this.listView1);
+            this.pnlAttributes.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlAttributes.Location = new System.Drawing.Point(0, 25);
+            this.pnlAttributes.Name = "pnlAttributes";
+            this.pnlAttributes.Size = new System.Drawing.Size(383, 595);
+            this.pnlAttributes.TabIndex = 1;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(383, 25);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 595);
+            this.splitter1.TabIndex = 2;
+            this.splitter1.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Location = new System.Drawing.Point(7, 20);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(373, 572);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Entites and Attributes";
+            // 
             // DateTimeBehaviorUtility
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.pnlAttributes);
             this.Controls.Add(this.toolStrip1);
             this.Name = "DateTimeBehaviorUtility";
-            this.Size = new System.Drawing.Size(404, 314);
+            this.Size = new System.Drawing.Size(907, 620);
+            this.ConnectionUpdated += new XrmToolBox.Extensibility.PluginControlBase.ConnectionUpdatedHandler(this.DateTimeBehaviorUtility_ConnectionUpdated);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            this.pnlAttributes.ResumeLayout(false);
+            this.pnlAttributes.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,5 +132,10 @@
 
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton btnClose;
+        private System.Windows.Forms.ToolStripButton btnLoadAttributes;
+        private System.Windows.Forms.Panel pnlAttributes;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Innofactor.XTB.DateTimeBehaviorUtility/DateTimeBehaviorUtility.cs
+++ Innofactor.XTB.DateTimeBehaviorUtility/DateTimeBehaviorUtility.cs
@@ -8,11 +8,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using XrmToolBox.Extensibility;
+using XrmToolBox.Extensibility.Interfaces;
 
 namespace Innofactor.XTB.DateTimeBehaviorUtility
 {
-    public partial class DateTimeBehaviorUtility : PluginControlBase
+    public partial class DateTimeBehaviorUtility : PluginControlBase, IGitHubPlugin
     {
+        public string RepositoryName { get { return "XrmToolBox.DateTimeBehaviorUtility"; } }
+
+        public string UserName { get { return "Innofactor"; } }
+
         public DateTimeBehaviorUtility()
         {
             InitializeComponent();
@@ -21,6 +26,23 @@ namespace Innofactor.XTB.DateTimeBehaviorUtility
         private void btnClose_Click(object sender, EventArgs e)
         {
             CloseTool();
+        }
+
+        private void DateTimeBehaviorUtility_ConnectionUpdated(object sender, ConnectionUpdatedEventArgs e)
+        {
+            btnLoadAttributes.Enabled = Service != null;
+        }
+
+        private void btnLoadAttributes_Click(object sender, EventArgs e)
+        {
+            LoadAttributes();
+        }
+
+        private void LoadAttributes()
+        {
+            var asyncinfo = new WorkAsyncInfo();
+            asyncinfo.Message = "Loading DateTime attributes";
+            WorkAsync(asyncinfo);
         }
     }
 }
