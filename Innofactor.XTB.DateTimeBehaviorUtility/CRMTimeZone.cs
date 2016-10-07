b05c1da6900b898b4ad798008dc5c91c391f4e21
Jonas Rapp <jonas.rapp@cinteros.se>
2016-10-07 15:11:44 +0200

    Added CRMTimeZone class

---
+++ Innofactor.XTB.DateTimeBehaviorUtility/CRMTimeZone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Innofactor.XTB.DateTimeBehaviorUtility
+{
+    internal class CRMTimeZone
+    {
+        public int TimeZoneCode { get; private set; }
+        public string Region { get; private set; }
+        public float Offset { get; private set; }
+
+        public CRMTimeZone(int timeZoneCode, string region, float offset)
+        {
+            TimeZoneCode = timeZoneCode;
+            Region = region;
+            Offset = offset;
+        }
+
+        public override string ToString()
+        {
+            return $"{Region} ({Offset})";
+        }
+    }
+}
